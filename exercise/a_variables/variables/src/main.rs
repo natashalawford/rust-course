b89bc94e1e7c188baa915ff93536363dbb63c9cc
natashalawford <natasha.lawford@gmail.com>
2025-05-23 18:43:17 -0400

    part a bonus completed

--- exercise/a_variables/variables/src/main.rs
+++ exercise/a_variables/variables/src/main.rs
@@ -3,11 +3,11 @@ const READY_AMOUNT: i32 = 2;
 
 fn main() {
     // PART 1
-    let mut missiles: i32 = STARTING_MISSILES;
-    let ready: i32 = READY_AMOUNT;
+    let (mut missiles, ready) : (i32, i32) = (STARTING_MISSILES, READY_AMOUNT);
     println!("Firing {} of my {} missiles...", ready, missiles);
     
     // PART 2
-    missiles = missiles - ready;
+    missiles = missiles - ready; // If I put this directly in the print statement
+                                 // below, missiles doesn't need to be mutable!
     println!("{} missiles left", missiles);
 }
